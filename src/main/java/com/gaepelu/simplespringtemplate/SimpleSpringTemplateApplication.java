bae0855e38ab1699c9e997ef1d59313aca76f6a3
g.peluso <g.peluso@ggeedu.it>
2023-11-08 16:36:05 +0100

    commit

---
+++ src/main/java/com/gaepelu/simplespringtemplate/SimpleSpringTemplateApplication.java
@@ -0,0 +1,13 @@
+package com.gaepelu.simplespringtemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringTemplateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringTemplateApplication.class, args);
+    }
+
+}
