3229ed6480aecef610f5677f4662a9388f64dc21
g.peluso <g.peluso@ggeedu.it>
2023-11-09 15:14:36 +0100

    update

--- src/test/java/com/gaepelu/simplespringtemplate/SimpleSpringTemplateApplicationTests.java
+++ src/test/java/com/gaepelu/simplespringtemplate/SimpleSpringTemplateApplicationTests.java
@@ -1,13 +1,36 @@
 package com.gaepelu.simplespringtemplate;
 
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@Slf4j
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureMockMvc
 class SimpleSpringTemplateApplicationTests {
+
+
+    @Autowired
+    private MockMvc mvc;
 
     @Test
     void contextLoads() {
+        log.info("context loads");
+    }
+
+    @WithMockUser(value = "spring")
+    @Test
+    void testApi() throws Exception {
+        mvc.perform(get("/schools").contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk());
     }
 
 }
